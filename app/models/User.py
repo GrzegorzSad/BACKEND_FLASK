d6ca323aa35c82017031b17b02d89f04e9dc256a
GrzegorzSad <N00223252@iadt.ie>
2024-11-19 18:11:12 +0100

    slide 41

--- app/models/User.py
+++ app/models/User.py
@@ -8,3 +8,10 @@ class User(db.Model):
     username: so.Mapped[int] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[int] = so.mapped_column(sa.String(128), index=True, unique=True)
     password_hash: so.Mapped[Optional[int]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+    
+from app.models.Post import Post
